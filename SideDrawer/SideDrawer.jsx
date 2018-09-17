332477ed0a7cfcfbafc2cea75ed618f14e0efb12
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-09-16 21:22:45 -0400

    adding files

---
+++ SideDrawer/SideDrawer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SideDrawer.css'
+
+const sideDrawer = props => {
+    let drawerClaseses = 'sideDrawer';
+    if(props.show) {
+        drawerClaseses ='sideDrawer open';
+    }
+  return (
+  <nav className={drawerClaseses}>
+        <ul>
+            <li><a href='/'>Stats</a></li>
+            <li><a href='/'>Placeholder</a></li>
+        </ul>
+    </nav>);
+};
+
+export default sideDrawer;
